e97935f4fcbdd86fd3eafd63cf36dd3f75b3fbc2
Chun Yang <canadachunyang@gmail.com>
2018-01-20 04:17:10 +0000

    user Authentication added

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,35 @@
+class UsersController < ApplicationController
+
+  def show
+    @user = User.find(params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to '/'
+    else
+      render :new
+    end
+  end
+
+  def edit
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(
+      :first_name,
+      :last_name,
+      :email,
+      :password
+    )
+  end
+end
