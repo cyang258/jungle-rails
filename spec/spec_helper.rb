c470b4ddcdf5375969bd6045e5727102f109c350
Chun Yang <canadachunyang@gmail.com>
2018-01-23 20:58:25 +0000

    test the home page

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,8 +20,8 @@ require 'factory_girl_rails'
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.strategy = :transaction
-    DatabaseCleaner.clean_with(:transaction)
+    DatabaseCleaner.strategy = :truncation
+    DatabaseCleaner.clean_with(:truncation)
   end
 
   config.around(:each) do |example|
